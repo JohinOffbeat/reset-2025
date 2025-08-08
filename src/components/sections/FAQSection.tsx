308f25d06a7f7bbf8a32016146439bed9fed121d
Offbeat Culture <offbeatculture@Offbeats-Mac-mini.local>
2025-08-08 22:03:33 +0530

    push 2

--- src/components/sections/FAQSection.tsx
+++ src/components/sections/FAQSection.tsx
@@ -28,11 +28,31 @@ const faqs = [
   },
   {
     question: "What time should I be there at the Venue?",
-    answer: "Please book your tickets in a way that you reach Bangalore on 3rd October late morning, please reach the venue by 1:30 PM. Please make sure that you've completed your lunch and you reach Leroy Grand because the Registration and Event will get started right away!"
+    answer: "Please book your tickets in a way that you reach Bangalore on 3rd October late morning, please reach the venue by 1:30 PM to 2:30 PM. Please make sure that you've completed your lunch and you reach Leroy Grand because the Registration and Event will get started right away!"
   },
   {
     question: "When can I book my tickets to go back home after Reset?",
     answer: "Please book your travel tickets to go back home for after 10:30 PM on 6th October (Monday) because we don't want you to rush after the event, or get stuck in traffic and miss your bus / train / flight. 10:30 PM would give you plenty of time to complete your last day peacefully, and reach your station / airport without missing any part of your experience"
+  },
+  {
+    question: "What is the Venue? And where is the Hotel that I will be staying in?",
+    answer: "Both Hotel and the Venue are the same - Leroy Grand, Yashwanthpur. You'll be spending 4 days here in total immersion!"
+  },
+   {
+    question: "What dates is Reset 2025 happening??",
+    answer: "Reset 2025 is happening on October 3, 4, 5, 6. [Friday to Monday]. Friday - make sure that you reach the venue by 2:30 PM. Please make sure that you've completed your lunch and you reach Leroy Grand because the Registration and Event will get started right away! Monday - Please book your travel tickets to go back home for after 10:30 PM on 6th October (Monday"
+  },
+  {
+    question: "If I need any help, who can I reach out to?",
+    answer: "For any help, please send a mail to info@swastiknandakumar.com. The team is active here - they will help you out within a few hours!"
+  },
+   {
+    question: "What are the closest travel options?",
+    answer: "Airport - Kempegowda International Airport, Bengaluru | 33 kms from Leroy Grand, Yashwanthpur Railway Station - Yesvantpur Railwau Station | 3.6 kms from Leroy Grand, Yashwanthpur Bus Stop - 5 kms from Leroy Grand, Yashwanthpur"
+  },
+  {
+    question: "Is the Reset Fee refundable?",
+    answer: "Since this is an Offline event, the amount you pay for Reset is directly sent to the hotel, as they have to block rooms for you. For this reason, the amount is non-refundable."
   },
 ];
 
